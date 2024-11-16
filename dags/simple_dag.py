53b6fae2df40da7150e78aa2183961f7306b1627
denka_nicee <denispopov200435@gmail.com>
2024-11-17 01:23:51 +0300

    Оно робит. Файлы скачиваются

--- dags/simple_dag.py
+++ dags/simple_dag.py
@@ -1,57 +1,56 @@
-# import os
-# from datetime import datetime
-#
-# import pandas as pd
-# from airflow import DAG
-# from airflow.operators.python import PythonOperator
-# from environs import Env
-#
-# env = Env()
-# env.read_env("../.env")
-# username = env.str("KAGGLE_USERNAME", None)
-# key = env.str("KAGGLE_KEY", None)
-#
-# Env.str("KAGGLE_USERNAME", None)
-# Env.str("KAGGLE_KEY", None)
-# from kaggle.api.kaggle_api_extended import KaggleApi
-#
-# def download_dataset():
-#     # Инициализация API Kaggle
-#     api = KaggleApi()
-#     api.authenticate()
-#
-#     # Загрузка датасета
-#     dataset_path = 'path/to/dataset'  # Замените на путь к вашему датасету на Kaggle
-#     download_path = '/tmp/kaggle_dataset'
-#     api.dataset_download_files(dataset_path, path=download_path, unzip=True)
-#
-#     # Чтение CSV файла
-#     csv_file = f'{download_path}/{os.listdir(download_path)[0]}'  # Предполагается, что в архиве только один CSV файл
-#     df = pd.read_csv(csv_file)
-#
-#     # Вывод первых 10 строк
-#     print(df.head(10))
-#
-# # Определение DAGа
-# default_args = {
-#     'owner': 'airflow',
-#     'start_date': datetime(2023, 10, 1),
-#     'retries': 1,
-# }
-#
-# dag = DAG(
-#     'download_kaggle_dataset',
-#     default_args=default_args,
-#     description='A simple DAG to download a dataset from Kaggle and print the first 10 rows',
-#     schedule_interval=None,
-# )
-#
-# # Определение задачи
-# download_task = PythonOperator(
-#     task_id='download_dataset',
-#     python_callable=download_dataset,
-#     dag=dag,
-# )
-#
-# # Установка порядка выполнения задач
-# download_task
+import os
+
+import pandas as pd
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import os
+import zipfile
+from environs import Env
+
+env = Env()
+env.read_env("../.env")
+username = env.str("KAGGLE_USERNAME", None)
+key = env.str("KAGGLE_KEY", None)
+import kaggle
+
+
+def download_dataset():
+    # Инициализация API Kaggle
+    api = kaggle.KaggleApi()
+    api.authenticate()
+
+    # Путь к датасету на Kaggle
+    dataset_path = 'antonkozyriev/game-recommendations-on-steam'
+    download_path = '/tmp/kaggle_dataset'
+
+    # Создание директории для загрузки, если она не существует
+    os.makedirs(download_path, exist_ok=True)
+
+    # Загрузка датасета
+    api.dataset_download_files(dataset_path, path=download_path, unzip=True)
+
+# Определение DAGа
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime.now() - timedelta(days=1),
+    'retries': 1,
+}
+
+dag = DAG(
+    'download_kaggle_dataset',
+    default_args=default_args,
+    description='A DAG to download multiple files (CSV and JSON) from Kaggle',
+    schedule_interval=None,  # Запуск только по триггеру
+    catchup=False,  # Не запускать прошлые даты
+)
+
+# Определение задачи
+download_task = PythonOperator(
+    task_id='download_dataset',
+    python_callable=download_dataset,
+    dag=dag,
+)
+
+# Установка порядка выполнения задач
+download_task
